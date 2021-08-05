9dd6cf86a351eb79dc2f3346c7b98f75839d6c77
Kevin Mukuna <mukuna.kevin@gmail.com>
2021-08-05 12:34:28 +0100

    alterd some multipl files

--- webpages/models.py
+++ webpages/models.py
@@ -2,7 +2,22 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.template.defaultfilters import slugify
 from django.db import models
-from embed_video.fields import EmbedVideoField
+
+CLOTHING_TYPE_CHOICES = [
+    ('Choice1', 'Choice1'),
+    ('Choice2', 'Choice2'),
+    ('Choice3', 'Choice'),
+    ('Choice4', 'Choice4'),
+
+]
+
+CLOTHING_METRIALS_TYPE_CHOICES = [
+    ('Choice1', 'Choice1'),
+    ('Choice2', 'Choice2'),
+    ('Choice3', 'Choice'),
+    ('Choice4', 'Choice4'),
+
+]
 
 
 def get_image_filename(instance, filename):
@@ -10,7 +25,7 @@ def get_image_filename(instance, filename):
     returns the path that contains the address of the post for the image
     """
     post = instance.post
-    address = "-".join(item for item in [post.title, post.question1, post.question2, post.question3] if item)
+    address = "-".join(item for item in [post.title, post.question1, post.question2] if item)
     slug = slugify(address)
     return "post_images/%s-%s" % (slug, filename)
 
@@ -21,18 +36,17 @@ class Post(models.Model):
     query
     the fields are defined below come teh model defined above
     """
-    summary = models.TextField(null=True, blank=True)
-    title = models.CharField(max_length=255, verbose_name="Topic Of The week")
-    question1 = models.CharField(max_length=255, verbose_name="Question 1")
-    question2 = models.CharField(max_length=255, verbose_name="Question 2", blank=True, null=False)
-    question3 = models.CharField(max_length=255, verbose_name="Question 3", blank=True, null=False)
-    video = EmbedVideoField()
-
+    summary = models.TextField(null=True, blank=True, verbose_name="Describe your product")
+    title = models.CharField(max_length=50, verbose_name="Give your ripple a title")
+    question1 = models.CharField(max_length=255, verbose_name="material type", choices=CLOTHING_METRIALS_TYPE_CHOICES)
+    question2 = models.CharField(max_length=255, verbose_name="Clothing type", choices=CLOTHING_TYPE_CHOICES)
+    question3 = models.IntegerField(verbose_name="Available quantity", blank=True, null=False)
     date_posted = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, related_name="user_posts", on_delete=models.CASCADE)
     display_q1 = models.CharField(max_length=255, null=False, blank=True, editable=False)
     display_q2 = models.CharField(max_length=255, null=False, blank=True, editable=False)
     display_q3 = models.CharField(max_length=255, null=False, blank=True, editable=False)
+
 
     class Meta:
         ordering = ['-id']
@@ -45,32 +59,28 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         """
-            this method return a string to redirect the user after posting to that
-            post by returning post details the primary key of the newly created
-            post
+        this method return a string to redirect the user after posting to that
+        post by returning post details the primary key of the newly created
+        post
 
-            the pk is used to identify each post, meaning when each post is creted,
-            it's assigned a pk number :return:
+        the pk is used to identify each post, meaning when each post is created,
+        it's assigned a pk number
+        :return:
         """
         return reverse('post-detail', kwargs={'pk': self.pk})
 
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
         """
-            overwriting the django save method
-            by passing in the estimated price into the price before it is saved
+        overwriting the django save method
+        by passing in the estimated price into the price before it is saved
         """
         display_q1 = self.question1 if 46 > len(self.question1) else (self.question1[:45]) + ".."
         display_q2 = self.question2 if self.question1 is not None or 46 > len(self.question2) else (self.question2[
                                                                                                     :45]) + ".."
-        display_q3 = self.question3 if self.question1 is not None or 46 > len(self.question3) else (self.question3[
-                                                                                                    :45]) + ".."
-
         title = self.title.upper()
         self.title = title
         self.display_q1 = display_q1
         self.display_q2 = display_q2
-        self.display_q3 = display_q3
-
         super().save(force_insert, force_update, *args, **kwargs)
 
 
--- webpages/views.py
+++ webpages/views.py
@@ -13,7 +13,6 @@ POST_FIELDS = [
     'question2',
     'question3',
     'summary',
-    'video',
 ]
 
 
@@ -38,28 +37,7 @@ class PostListView(ListView):
     paginate_by = 5
 
 
-class UserPostListView(LoginRequiredMixin, ListView):
-    """
-        model: name of model, in this case post
-        template_name: name of templates, in this case user_posts.html
-        context_object_name: name of the object u want in the template in this case posts'
-        paginate_by: how many post you want in each page, in this case 5
-
-    """
-    model = Post
-    template_name = 'dashboard/dashboard_post.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        """
-            this function get the user if they exist else it returns 404 --> this function is used to filter and display
-            a requested user
-            filter by the user requested and sort by the latest post
-        """
-        return Post.objects.filter(author=self.request.user).order_by('-date_posted')
-
-
-class PostDetailView(DetailView, FormMixin):
+class PostDetailView(DetailView):
     """
     detail view, as as above but just using the generic django conventions
     <appname>/<model>_<viewtype>.html
@@ -70,28 +48,6 @@ class PostDetailView(DetailView, FormMixin):
     """fix this to render properly """
     model = Post
 
-    # template_name = "webpages/post_detail.html"
-    # data = CommentForm()
-
-    def get_success_url(self):
-        return reverse('post-detail', kwargs={'pk': self.object.id})
-
-    def get_context_data(self, **kwargs):
-        context = super(PostDetailView, self).get_context_data(**kwargs)
-        return context
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        form.save()
-        return super(PostDetailView, self).form_valid(form)
-
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     """
@@ -100,6 +56,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     fields: as define in the model, in this case POST_FIELDS
     """
     model = Post
+    fields = POST_FIELDS
 
     def form_valid(self, form):
         """ this function allows user to create a new post if they login """
@@ -161,36 +118,3 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
-
-
-# def post_detail(request, year, month, day, post):
-#     # co oparate this with the one above
-#     post = get_object_or_404(Post, slug=post,
-#                              status='published',
-#                              publish__year=year,
-#                              publish__month=month,
-#                              publish__day=day)
-#
-#     # List of active comments for this post
-#     comments = post.comments.filter(active=True)
-#
-#     new_comment = None
-#
-#     if request.method == 'POST':
-#         # A comment was posted
-#         comment_form = CommentForm(data=request.POST)
-#         if comment_form.is_valid():
-#             # Create Comment object but don't save to database yet
-#             new_comment = comment_form.save(commit=False)
-#             # Assign the current post to the comment
-#             new_comment.post = post
-#             # Save the comment to the database
-#             new_comment.save()
-#     else:
-#         comment_form = CommentForm()
-#     return render(request,
-#                   'blog/post/detail.html',
-#                   {'post': post,
-#                    'comments': comments,
-#                    'new_comment': new_comment,
-#                    'comment_form': comment_form})
